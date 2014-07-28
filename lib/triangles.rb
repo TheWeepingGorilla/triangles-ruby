6abdd7303b2f1bd5f37e85c8bac1c361d27755d7
Sean Morrison & Sterling Houghton <student@epicodus.com>
2014-07-28 12:27:38 -0700

    Passes second spec.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,2 +1,20 @@
 class Triangle
+  def initialize (side1, side2, side3)
+  @side1 = side1
+  @side2 = side2
+  @side3 = side3
+  end
+
+  def side1
+    @side1
+  end
+
+  def side2
+    @side2
+  end
+
+  def side3
+    @side3
+  end
+
 end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -3,7 +3,11 @@ require 'triangles'
 
 describe "Triangle" do
   it "creates an instance of class triangle" do
-    test_triangle = Triangle.new
+    test_triangle = Triangle.new(2,3,4)
     test_triangle.should be_an_instance_of Triangle
+  end
+  it "creates a triangle with three sides" do
+    test_triangle = Triangle.new(2,3,4)
+    test_triangle.side1.should eq 2
   end
 end
